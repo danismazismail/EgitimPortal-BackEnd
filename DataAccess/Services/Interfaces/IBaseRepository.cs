397295bcb1e8d02906c94bbefd513876669efd73
Ismail DANISMAZ <danismazismail@gmail.com>
2024-07-09 17:17:07 +0300

    ismail=>Added-IBaseRepository

--- DataAccess/Services/Interfaces/IBaseRepository.cs
+++ DataAccess/Services/Interfaces/IBaseRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Core.Entities.Abstract;
+using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +11,30 @@ namespace DataAccess.Services.Interfaces
 {
     public interface IBaseRepository<T> where T : BaseEntity
     {
+        //Creat Update Delete Operations
+        Task<bool> AddAsync(T entity);
+
+        Task<bool> UpdateAsync(T entity);
+
+        Task<bool> DeleteAsync(T entity);
+
+        Task<bool> SaveAsync();
+
+        //Read Operations
+        Task<T> GetByIdAsync(string id);
+
+        Task<T> GetByDefaultAsync(Expression<Func<T, bool>> expression);
+
+        Task<ICollection<T>> GetByDefaultsAsync(Expression<Func<T, bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task<ICollection<TResult>> GetFilteredListAsync<TResult>
+            (
+                Expression<Func<T, TResult>> select,
+                Expression<Func<T , bool>> where = null,
+                Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                Func<IQueryable<T> , IIncludableQueryable<T, object>> join = null 
+            );
     }
 }
